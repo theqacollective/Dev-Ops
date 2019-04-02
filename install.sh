8eed391a9832199cd736950f52aa102f768b3d8b
Kyle Warrington <47660158+Kyrvix@users.noreply.github.com>
2019-04-02 15:35:27 +0100

    Changed Eureka to access Master

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/theqacollective/Eureka-Api -b Development
+git clone https://github.com/theqacollective/Eureka-Api
 git clone https://github.com/theqacollective/Rooms-Api -b Development
 git clone https://github.com/theqacollective/Tenant-Api -b Development
 git clone https://github.com/theqacollective/Building-Api -b Development

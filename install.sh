dfa6718bce2cf9aa1b9ba7bc3e69bc21b9b55e11
Kyle Warrington <47660158+Kyrvix@users.noreply.github.com>
2019-04-02 15:16:14 +0100

    Install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/theqacollective/Eureka-Api
+git clone https://github.com/theqacollective/Rooms-Api
+git clone https://github.com/theqacollective/Tenant-Api
+git clone https://github.com/theqacollective/Building-Api
+git clone https://github.com/theqacollective/Maintenance-Api
+git clone https://github.com/theqacollective/Landlord-Api
+git clone https://github.com/theqacollective/Front-End
+
+docker-compose up -d

4c1853bed71fca3b673c95b0a813c2349623e731
Kyle Warrington <47660158+Kyrvix@users.noreply.github.com>
2019-04-02 15:30:34 +0100

    Cloning Development Branch

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/theqacollective/Eureka-Api
-git clone https://github.com/theqacollective/Rooms-Api
-git clone https://github.com/theqacollective/Tenant-Api
-git clone https://github.com/theqacollective/Building-Api
-git clone https://github.com/theqacollective/Maintenance-Api
-git clone https://github.com/theqacollective/Landlord-Api
-git clone https://github.com/theqacollective/Front-End
+git clone https://github.com/theqacollective/Eureka-Api -b Development
+git clone https://github.com/theqacollective/Rooms-Api -b Development
+git clone https://github.com/theqacollective/Tenant-Api -b Development
+git clone https://github.com/theqacollective/Building-Api -b Development
+git clone https://github.com/theqacollective/Maintenance-Api -b Development
+git clone https://github.com/theqacollective/Landlord-Api -b Development
+git clone https://github.com/theqacollective/Front-End -b Development
 
 docker-compose up -d

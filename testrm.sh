722a486eb0dfe14848aa58e6a90ee427f0842242
Kyle Warrington <47660158+Kyrvix@users.noreply.github.com>
2019-04-03 12:09:25 +0100

    Script to remove Tests

---
+++ testrm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo rm -r /home/scyther7777/accommodation/Dev-Ops/Building-Api/src/test
+sudo rm -r /home/scyther7777/accommodation/Dev-Ops/Eureka-Api/src/test
+sudo rm -r /home/scyther7777/accommodation/Dev-Ops/Maintenance-Api/src/test
+sudo rm -r /home/scyther7777/accommodation/Dev-Ops/Tenant-Api/src/test
+sudo rm -r /home/scyther7777/accommodation/Dev-Ops/Landlord-Api/src/test
+sudo rm -r /home/scyther7777/accommodation/Dev-Ops/Rooms-Api/src/test
